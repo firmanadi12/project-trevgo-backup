2e898f69d22607dccfed14254779b34d0789d10c
sebastianbk98 <sebastianbk007@gmail.com>
2022-07-05 12:27:51 +0700

    Add seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Admin Trevgo',
+            'email' => 'admin@trevgo.com',
+            'password' => Hash::make('admin123'),
+            'roles' => 'ADMIN',
+            'username' => 'admin',
+            'email_verified_at' => now()
+
+        ]);
     }
 }
